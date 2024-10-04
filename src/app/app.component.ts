0e4be817e2b7b7fa02f188fe4b2970ca885d4efb
AMichelas <120266869+AMichelas@users.noreply.github.com>
2024-10-04 13:16:28 +0200

    work on navbar darkmode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,6 @@ export class AppComponent {
   isLoggedIn = false;
   showAdminBoard = false;
   username?: string;
-  isDarkMode = false; // État du mode nuit
 
   eventBusSub?: Subscription;
 
@@ -47,12 +46,5 @@ export class AppComponent {
 
   goBack(): void {
     window.history.back();
-  }
-
-  toggleDarkMode(): void {
-    this.isDarkMode = !this.isDarkMode; // Basculer l'état du mode nuit
-    console.log('Mode nuit activé:', this.isDarkMode); // Vérifiez l'état
-    const body = document.body;
-    body.classList.toggle('dark-mode', this.isDarkMode); // Ajouter ou retirer la classe de mode nuit
   }
 }
